13c21e5911eeeaf0a583bfa2a96843094d0c7635
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-08-09 00:01:18 +0300

    Shell teamwork

--- string_func.c
+++ string_func.c
@@ -34,3 +34,67 @@ int _strlen(char *s)
 	}
 	return (count);
 }
+
+/**
+ * _strncmp - compares two strings up to n bytes
+ * @s1: compared to s2
+ * @s2: compared to s1
+ * @n: number of bytes
+ * Return: difference between s1 and s2
+ */
+int _strncmp(char *s1, char *s2, int n)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+/**
+ * _strdup - dupicates string
+ * @s: to be duplicated
+ * Return: pointer to duplicate string
+ */
+char *_strdup(char *s)
+{
+	char *ptr;
+	int i, len;
+
+	if (s == NULL)
+		return (NULL);
+
+	len = _strlen(s);
+
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; *s != '\0'; s++, i++)
+		ptr[i] = s[0];
+
+	ptr[i++] = '\0';
+	return (ptr);
+}
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be checked
+ * @c: character to be located
+ * Return: returns pointer to first occurence of character
+ * or NULL if character not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (!c)
+		return (s);
+	return (NULL);
+}
